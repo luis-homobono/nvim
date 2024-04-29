50ed87d36794bfa704dd838582cf14c6f754dcfa
Luis Eloy Homobono Fuentes <eloy.homobono@gmail.com>
2024-04-28 22:54:15 -0600

    add nvim maximiser

---
+++ lua/luiseloy/plugins/nvim-maximizer.lua
@@ -0,0 +1,7 @@
+return {
+  -- plugin for maximizing spliced buffers
+  "szw/vim-maximizer",
+  keys = {
+    { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
+  },
+}
