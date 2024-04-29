3e1623ef116f2da920b422bf2e8033e6ab5e514f
Luis Eloy Homobono Fuentes <eloy.homobono@gmail.com>
2024-04-29 16:21:54 -0600

    add formating and trouble plugins

--- lua/luiseloy/plugins/lsp/mason.lua
+++ lua/luiseloy/plugins/lsp/mason.lua
@@ -3,6 +3,7 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     -- import mason
@@ -10,6 +11,8 @@ return {
 
     -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
+
+    local mason_tool_installer = require("mason-tool-installer")
 
     -- enable mason and configure icons
     mason.setup({
@@ -47,6 +50,15 @@ return {
 				-- "r_language_server", -- r
 				"sqlls", -- sql
 				"typos_lsp", -- all
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
       },
     })
   end,
