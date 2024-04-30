fc72bb781e2813053ece0d8c769fdba7e2f0e751
Luis Eloy Homobono Fuentes <eloy.homobono@gmail.com>
2024-04-29 18:39:08 -0600

    add gitsigns plugin
    add keymaps for processing

--- lua/luiseloy/core/keymaps.lua
+++ lua/luiseloy/core/keymaps.lua
@@ -2,8 +2,9 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk"})
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("n", "<leader>nh", "<cmd>nohl<CR>", { desc = "Clear search highlights" })
+-- keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
 
 -- increment / decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
@@ -19,11 +20,23 @@ keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }
 keymap.set("n", "<leader><tab><tab>", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 keymap.set("n", "<S-h>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<Tab>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<S-l>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<S-tab>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+-- Move Lines
+keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
 -- general keys
 keymap.set({ "n", "i", "x", "s" }, "<C-s>", "<cmd>w<CR><ESC>", { desc = "Save file" })
 keymap.set("n", "<leader>l", "<cmd>Lazy<CR>", { desc = "Lazy" })
 keymap.set("n", "<leader>qq", "<cmd>qa<CR>", { desc = "Exit nvim" })
 keymap.set("n", "<leader>m", "<cmd>Mason<CR>", { desc = "Mason" })
+keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all text" })
